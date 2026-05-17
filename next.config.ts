6effa31ab48dfae173418c18a104d4f75e5dcaea
unknown <missikazi145@gmail.com>
2026-05-17 19:00:03 +0530

    chore: upgrade next.js to 16.2.6 to fix CVE-2025-55182 security vulnerability

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['tesseract.js'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
